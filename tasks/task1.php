a3a73735c107f2f6570d5c99fd2a252cac6041f9
Vasyabest <Vasyabest@rambler.ru>
2017-07-04 11:53:39 +0300

    1th task complitly_refactor

--- tasks/task1.php
+++ tasks/task1.php
@@ -1,24 +1,51 @@
 <?php
-$w=4;
-$h=5;
-$s="*";
-function chess($width,$height,$s){
+$w = 6;
+$h = 8;
+$s = "*";
 
-    for($i=0;$i<$width;$i++) {
-        for($j=0;$j<$height;$j++){
-            $arr[$i][$j]=$s;
-            if (($i%2)==0){
-                echo $arr[$i][$j], "&nbsp";
-            }
-            if (($i%2)==1){
-                echo "&nbsp",$arr[$i][$j];
-            }
-        }
-        echo "</br>";
+function checkValidation($width, $height)
+{
+
+    if (is_int($width) && is_int($height) && ($width > 0) && ($height > 0)) {
+        $result = true;
+    } else {
+        $result = false;
     }
+    return $result;
+
+}
+
+function drawingBoard($width, $height, $s)
+{
+    $space = "&nbsp";
+    $break = "</br>";
+
+    $chess_board = "";
+    for ($i = 0; $i < $width; $i++) {
+        for ($j = 0; $j < $height; $j++) {
+            if (($i % 2) == 0) {
+                $chess_board = $chess_board . $s . $space;
+            } elseif (($i % 2) == 1) {
+                $chess_board = $chess_board . $space . $s;
+            }
+
+        }
+        $chess_board = $chess_board . $break;
+    }
+    return $chess_board;
 }
 
 
+function startDrawing($width, $height, $s)
+{
+    if (!checkValidation($width, $height)) {
+        $result = (object)array('status' => 'failed', 'reason' => 'You have to enter width and height more than 0');
+    } else {
+        $result = drawingBoard($width, $height, $s);
+    }
+    return $result;
+}
 
-chess($w,$h,$s);
+/*echo(startDrawing($w, $h, $s));*/
 ?>
+
