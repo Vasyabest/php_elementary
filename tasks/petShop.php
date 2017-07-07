251da3654adf7ce32e7837bb86de97e383ee6e00
Vasyabest <Vasyabest@rambler.ru>
2017-07-07 15:56:01 +0300

    petShop added

--- tasks/petShop.php
+++ tasks/petShop.php
@@ -2,17 +2,25 @@
 
 class PetShop
 {
-    public function catsNumber()
+    private $pets = [];
+
+    protected function addPet($newPet)
+    {
+        $this->pets[] = $newPet;
+    }
+
+
+    public function getCats()
     {
        
     }
 
-    public function moreAvaragePrice()
+    public function getExpensivePets()
     {
         
     }
 
-    public function whiteOrFluffyPets()
+    public function getWhiteOrFluffyPets()
     {
 
     }
@@ -20,28 +28,21 @@ class PetShop
 
 abstract class Pet
 {
-
-    abstract protected function isFluffy();
-
     protected $price;
     protected $color;
 
-    function __construct($price, $color)
-    {
-        $this->edible = $price;
-        $this->color = $color;
-    }
+    abstract public function isFluffy();
+    abstract public function isName();
 
-    public function getPrice()
+    public function isPrice()
     {
         return $this->price;
     }
 
-    public function getColor()
+    public function isColor()
     {
         return $this->color;
     }
-
 }
 
 class Dog extends Pet
@@ -49,15 +50,24 @@ class Dog extends Pet
 
     private $name;
 
+    public function __construct($name, $color, $price){
+        $this->name = $name;
+        $this->color = $color;
+        $price->price = $price;
+    }
+
     public function isFluffy()
     {
         return $this->false;
     }
 
-    public function getName()
+    public function isName()
     {
-       return $this->name;
+        return $this->name;
     }
+
+
+
 }
 
 class Cat extends Pet
@@ -65,30 +75,44 @@ class Cat extends Pet
 
     private $name;
 
-    public function isFluffy()
-    {
-        return $this->true;
+    public function __construct($name, $color, $price, $fluffy){
+        $this->name = $name;
+        $this->color = $color;
+        $this->price = $price;
+        $this->fluffy = $fluffy;
     }
 
-    public function getName($name)
+    public function isFluffy()
     {
-        $this->name = $name;
+        return $this->fluffy;
+    }
+
+    public function isName()
+    {
+        return $this->name;
     }
 
 }
 
 class Hamster extends Pet
 {
+    public function __construct($color, $price){
+        $this->color = $color;
+        $this->price = $price;
+    }
 
     public function isFluffy()
     {
         return $this->true;
     }
 
-    public function getName($name)
+    public function isName()
     {
-        $this->name = $name;
+        return $this->false;
     }
+
 }
 
 ?>
+
+
