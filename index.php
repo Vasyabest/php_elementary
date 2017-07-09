5d08caadefcfbe39308f2c94dbc46df16d987f31
Vasyabest <Vasyabest@rambler.ru>
2017-07-09 22:39:30 +0300

    refactor structure

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include 'tasks\task1.php';
-include 'tasks\task2.php';
-include 'tasks\task3.php';
-include 'tasks\task4.php';
-include 'tasks\task5.php';
-?>
---
+++ tasks/index.php
@@ -0,0 +1,7 @@
+<?php 
+include 'tasks/task1.php';
+include 'tasks/task2.php';
+include 'tasks/task3.php';
+include 'tasks/task4.php';
+include 'tasks/task5.php';
+?>
